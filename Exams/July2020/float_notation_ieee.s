e34c857b78bd04813100b8e8adbd526ffa5acca1
TazTornado <sotiapantazi@gmail.com>
2021-06-09 23:45:26 +0300

    fixed mistakes in float notation program

--- Exams/July2020/float_notation_ieee.s
+++ Exams/July2020/float_notation_ieee.s
@@ -1,7 +1,7 @@
 #####################################
 # 					                #
 #       June 2020 - Group A Ex1     #
-# 			  		    			#		   
+# 			  		    			#
 #####################################
 # The task:
 # Read a float and then display its #
@@ -31,28 +31,28 @@ read_float:
 
 	li $v0, 2		    # print float
 	syscall
-	
-	c.eq.s $f0, $f1 
+
+	c.eq.s $f0, $f1
 	bc1t Exit           # if float is zero, exit
-	
+
 	la $a0, endl
 	li $v0, 4
-	syscall 
+	syscall
 
 	mfc1 $t0, $f0       # move float to an int reg, to process bits
 
 	srl $t1, $t0, 31    # isolating the sign
 
-	li $t5, ' '
+	li $t5, ' '         # t5 = 32
 	sb  $t5, sign       # fix the sign space in memory, from last loop
 	beqz $t1, continue  # if sign == 1 then store '-', if sign == 0 then leave blank
-storeMinus:             
+storeMinus:
 	li $t5, '-'
 	sb $t5, sign
 
 continue:
 	sll $t3, $t0, 1		# isolating the biased exponent in $t3
-	srl $t3, $t3, 24	
+	srl $t3, $t3, 24
 
 	li $t2, 127
 	sub $s0, $t3, $t2   # removing bias
@@ -63,24 +63,25 @@ continue:
 
 	li $t6, 22  # will be used as offset
 
-	loop:   
+	loop:
 		blt $t6, $zero, printAnsw   # escape clause
-		andi $t7, $t1, 1            # masking t1 to get 1st bit
+		andi $t7, $t4, 1            # masking t1 to get 1st bit
 		beqz $t7, storeZero
 
 	storeOne:
 		li $s1, '1'		        # store ascii for 1
 		sb $s1, mantissa($t6)
-		addi $t6, $t6, -1       # subtract from the counter 
-		j loop 
+		addi $t6, $t6, -1       # subtract from the counter
+        srl $t4, $t4, 1
+		j loop
 
 	storeZero:
 		li $s1, '0'		        # store ascii for 0
 		sb $s1, mantissa($t6)
-		sub $t6, $t6, $t8       # subtract from the counter 
-		srl
+		addi $t6, $t6, -1       # subtract from the counter
+		srl $t4, $t4, 1
 		j loop
-		
+
 
 printAnsw:
 	la $a0, answ		# print prompt for result
@@ -93,7 +94,7 @@ printAnsw:
 
 	la $a0, comma       # print comma
 	li $v0, 4
-	syscall	
+	syscall
 
 	la $a0, mantissa    # print mantissa
 	li $v0, 4
@@ -103,20 +104,26 @@ printAnsw:
 	li $v0, 4
 	syscall
 
+
 	la $a0, exponent    # print exponent
+    lw $a0, 0($a0)
 	li $v0, 1
 	syscall
 	la $a0, endl
 	li $v0, 4
-	syscall 
+	syscall
 
 
 	j read_float        # read next float
 
 
-Exit:	
+Exit:
+    la $a0, bye 		# print base
+    li $v0, 4
+    syscall
+
 	li $v0, 10
-	syscall																	
+	syscall
 
 .data
 mantissa: 	.space 24 			# 24 bytes for mantissa
@@ -128,4 +135,4 @@ base:	.asciiz " x 2^"
 exponent: 	.word 1     # just space for exponent
 comma:	.asciiz "1,"
 sign: 	.asciiz " "     # 1 byte for sign
-
+bye:    .ascii "\nBye.."
--- on_the_fly.s
+++ on_the_fly.s
@@ -1,22 +1,56 @@
 .data 
-read_int:   .asciiz "Enter a number: "
+read_str:   .space 3
+stored_str:	.space 3
 div_promptA:    .asciiz " is a divisor of "
 div_promptB:    .asciiz " is the min divisor of "
 
 .text
 
-taskA:  li $v0, 4       # read_int prompt
-        la $a0, read_int
-        syscall
+# taskA:  li $v0, 4       # read_int prompt
+#         la $a0, read_int
+#         syscall
 
-        li $a0, '\n'    # endl character
-        li $v0, 11      # print_char
-        syscall         # change line
+#         li $a0, '\n'    # endl character
+#         li $v0, 11      # print_char
+#         syscall         # change line
 
-        li $a0, 0xa
-        li $v0, 1
-        syscall
+#         li $a0, 0xa
+#         li $v0, 1
+#         syscall
+
+
+
+		li $v0, 8			# read a 2-char string
+		la $a0, read_str
+		li $a1, 3
+		syscall
+
+		lb $t1, read_str + 1	# load 1st byte - signed
+		lbu $t2, read_str + 1	# load same byte - unsigned
+
+		li $v0, 1
+		or $a0, $zero, $t1
+		syscall				# print signed
+
+		li $v0, 11
+		li $a0, '\n'
+		syscall 			# change line
+
+		li $v0, 1
+		or $a0, $zero, $t2
+		syscall				# print unsigned
+
+		li $v0, 11
+		li $a0, '\n'
+		syscall 			# change line
+
+		sb $t1, stored_str
+		
+		li $v0, 4
+		la $a0, stored_str
+		syscall
+
+
 
         li $v0, 10
         syscall         # exit routine
-
